071188cfb9cff439587d0c58acbb9b55a925eded
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-05-08 17:23:15 +0600

    update the profile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+from django.conf import settings
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/',blank=True,default='default.jpg')
+    def __str__(self):
+        return  f'{self.user.username} Profile'
--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -119,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
+
+CRISPY_TEMPLATE_PACK='bootstrap4'
+
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
